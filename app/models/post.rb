68a737b4f9b44893e8c1d4ced74cc52a7dd989fb
松本卓也 <takuyan@matsumototakuyanoMacBook-Air.local>
2021-03-08 04:16:16 +0900

    show validations mesage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   has_rich_text :content
 
-  validates :title, length: { maximum: 32 }, presence: true
+  validates :title, length: { maximum: 16 }, presence: true
 end
